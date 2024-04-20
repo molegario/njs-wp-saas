bbf1c76ebb5c04c1d57687437bb538a7128b8e68
molegario <molegario@x2omedia.com>
2024-04-20 14:43:22 -0400

    refactored search endpoint to use post

--- components/PropertySearch/PropertySearch.js
+++ components/PropertySearch/PropertySearch.js
@@ -1,24 +1,53 @@
 "use client";
 
 import { useCallback, useEffect, useState } from "react";
-import useSWR from "swr";
 import Results from "./Results/Results";
 import PagingNavigation from "components/PagingNavigation/PagingNavigation";
 import { useRouter } from "next/router";
 const PAGE_SIZE = 3;
+
+export const fetcher = async (
+  url,
+  payload,
+) => {
+  const options = {
+    method: payload ? "POST" : "GET",
+    ...(payload && { body: payload }),
+    headers: {
+      accept: "application/json",
+      "Content-Type": "application/json",
+    },
+  };
+  return fetch(url, options).then(r => r.json());
+};
 
 const PropertySearch = (props) => {
   const [properties, setProperties] = useState([])
   const [pages, setPages] = useState([])
   const router = useRouter()
   const reqOffset = router?.query?.pageoffset ?? 0
+  const [data, setData] = useState({})
 
-  const {data, error} = useSWR(
-    `/api/search?offset=${reqOffset}&pagesize=${PAGE_SIZE}`,
-    (url) => fetch(url).then(res => res.json()),
-    {
-      dedupingInterval: 0
-    }
+  const searchPages = useCallback(
+    async () => {
+      const resp = await fetch(
+        '/api/search',
+        {
+          method: 'POST',
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            offset: reqOffset,
+            pagesize: PAGE_SIZE,
+          })
+        }
+      );
+      const respJSON = await resp.json()
+      setData(respJSON)
+    },
+    [setData, reqOffset, PAGE_SIZE]
   )
 
   const processData = useCallback(
@@ -37,6 +66,13 @@ const PropertySearch = (props) => {
     [
       setProperties, setPages
     ]
+  )
+
+  useEffect(
+    () => {
+      searchPages()
+    },
+    [searchPages]
   )
 
   useEffect(
--- pages/api/search.js
+++ pages/api/search.js
@@ -3,7 +3,7 @@ import { getPropertyPages } from "utils/getPropertyPages";
 const FALLBACK_PAGE_SIZE = 1;
 
 const handler = async (req, res) => {
-  let { offset=0, pagesize=FALLBACK_PAGE_SIZE } = req.query;
+  let { offset=0, pagesize=FALLBACK_PAGE_SIZE } = req.body;
   try {
     const data = await getPropertyPages(+offset, +pagesize);
     res.status(200).json({ 
