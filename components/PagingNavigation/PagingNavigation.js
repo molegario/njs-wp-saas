0233d98abc6e03eaba408dc9daa9dc842b3e8210
molegario <molegario@x2omedia.com>
2024-04-20 13:13:12 -0400

    fixes to styling

--- components/PagingNavigation/PagingNavigation.js
+++ components/PagingNavigation/PagingNavigation.js
@@ -1,10 +1,9 @@
 const PagingNavigation = ({pages=[], handlePaging=()=>{}, pagenum=''}) => {
-  console.log("PAGES::PAGENUM", pages, pagenum)
   return ( 
     <div className="max-w-5xl mx-auto mb-2 flex justify-center gap-2">
       {
         pages.map((page, idx) => <button 
-          className={`btn shadow-sm h-[50px] min-w-[50px] ${+pagenum === +page ? "text-pink-500 bg-white border-2 border-pink-500" : "border-2 border-pink-500"}`} 
+          className={`btn shadow-sm h-[50px] min-w-[50px] ${+pagenum === +page ? "border-2 border-pink-500" : "text-pink-500 bg-white border-2 border-pink-500"}`} 
           key={`page-${idx}`} 
           onClick={handlePaging(page)}
         >{idx + 1}</button>)
